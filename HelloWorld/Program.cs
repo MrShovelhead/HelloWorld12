a59565708842ba531e36434d3259202c824d8a1d
Jakob Foersom <jfo@zbc.dk>
2025-08-09 10:05:35 +0200

    fsaf
    fsafsf

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(addition(2, 2));
+            //Console.ReadKey();
+        }
+        static int addition(int numberOne, int numberTwo)
+        {
+            int sum = numberOne + numberTwo;
+            return sum;
         }
     }
 }
